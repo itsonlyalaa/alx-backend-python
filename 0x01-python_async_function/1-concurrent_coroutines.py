d66c16f7d027ddbbc0c6318c0c55a32975bc394b
Alaa Okair <alaaokair330@gmail.com>
2024-08-06 02:51:20 +0000

    task2

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""takes in 2 int arguments, and waits for random delay """
+
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """return the list of all the delays"""
+    Delays_list: List[float] = []
+    Tasks_list: List = []
+
+    for x in range(n):
+        Tasks_list.append(wait_random(max_delay))
+
+    for task in asyncio.as_completed((Tasks_list)):
+        delay = await task
+        Delays_list.append(delay)
+
+    return Delays_list
